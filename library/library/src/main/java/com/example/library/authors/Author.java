11b32f488e16a75a2bd304ec0e618c8d2ad896ba
Sohail Khan <Khansohail0540@gmail.com>
2020-05-02 22:05:20 +0530

    just added no arguement and all arguement annotations

--- library/library/src/main/java/com/example/library/authors/Author.java
+++ library/library/src/main/java/com/example/library/authors/Author.java
@@ -5,6 +5,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Author {
 
